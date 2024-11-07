2333d7897181b29e387afec10e320c0f1c8a8eca
07Fahim10 <kingfahim7star143@gmail.com>
2024-11-07 17:55:10 +0600

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'plus-jakarta': ['"Plus Jakarta Sans", sans-serif;'],
+      }
+    },
   },
   plugins: [],
 }
